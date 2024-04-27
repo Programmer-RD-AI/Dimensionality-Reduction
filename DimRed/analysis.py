6f31cae3d5e1e639e3bb518515f02645dc73eb23
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-27 23:17:22 +0530

    Update analysis.py

--- DimRed/analysis.py
+++ DimRed/analysis.py
@@ -2,5 +2,15 @@ from DimRed import *
 
 
 class Analysis:
-    def __init__(self, standard_pipeline, pipeline, data):
+    def __init__(self, X: np.array, y: np.array, standard_pipeline=None, pipeline=None):
+        self.standard_pipeline = standard_pipeline
+        self.pipeline = pipeline
+        # self.standard_pipeline.fit(X)
+        # self.pipeline.fit(X)
+        self.X = X[0]
+        self.size_per_side = int(math.sqrt(len(self.X)))
+        self.X = self.X
+        self.y = y[0].reshape(1, -1)
+
+    def produce_combinations(self, name: str, standard_pipeline: Pipeline, pipeline: Pipeline) -> None:
         pass
