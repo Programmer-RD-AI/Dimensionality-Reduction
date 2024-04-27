7f4810e15008a6296c6c68594445ba11faab0b31
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-27 23:17:31 +0530

    Update variation.py

--- DimRed/variation/variation.py
+++ DimRed/variation/variation.py
@@ -2,8 +2,9 @@ from DimRed import *
 
 
 class _Variation:
-    def __init__(self,  standard_pipeline: Pipeline) -> None:
+    def __init__(self,  standard_pipeline: Pipeline, analysis_instance: Analysis) -> None:
         self.standard_pipeline = standard_pipeline
+        self.analysis_instance = analysis_instance
 
     def create_variation(self, param_grid: Dict, reduction_method: Any, reduction_method_name: str) -> List:
         grid = ParameterGrid(param_grid)
@@ -13,5 +14,7 @@ class _Variation:
             var = [(reduction_method_name, reduction_method(**params))]
             pipeline = Pipeline(
                 [step for step in self.standard_pipeline.steps] + var)
+            self.analysis_instance.produce_combinations(
+                reduction_method_name, self.standard_pipeline, pipeline)
             variations.append(pipeline)
         return variations
