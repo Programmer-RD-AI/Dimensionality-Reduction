c4045fe3ba0f202c61258199c5d4e94f2d6da6f8
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-29 22:51:48 +0530

    Update helper_functions.py

--- DimRed/helper_functions.py
+++ DimRed/helper_functions.py
@@ -1,11 +1,14 @@
 from DimRed import *
 
 
-def load_dataset(test_split: float = 0.25) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+def load_dataset(
+    test_split: float = 0.25,
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     digits = datasets.load_digits()
     X, y = digits.data, digits.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_split, stratify=y,
-                                                        random_state=RANDOM_STATE, shuffle=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_split, stratify=y, random_state=RANDOM_STATE, shuffle=True
+    )
     return X_train, X_test, y_train, y_test
 
 
@@ -23,7 +26,16 @@ def average_metric(metric, dictionaries: List[Dict[str, Union[str, int]]]) -> fl
     return float(avg / len(dictionaries))
 
 
-def add_to_dictionary(dictionary: Dict[str, List[Union[str, int]]], list_of_values: List[Union[str, int]]) -> Dict[str, List[Union[str, int]]]:
+def add_to_dictionary(
+    dictionary: Dict[str, List[Union[str, int]]], list_of_values: List[Union[str, int]]
+) -> Dict[str, List[Union[str, int]]]:
     for idx, key in enumerate(dictionary):
         dictionary[key].append(list_of_values[idx])
     return dictionary
+
+
+def director_exist(path):
+    # Create the directory if it does not exist
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
