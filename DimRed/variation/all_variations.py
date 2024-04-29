e686e83c261d7897a64177674ed1b31e5835d626
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-04-29 22:51:51 +0530

    Update all_variations.py

--- DimRed/variation/all_variations.py
+++ DimRed/variation/all_variations.py
@@ -3,14 +3,24 @@ from DimRed.variation.variation import _Variation
 
 
 class Variations(object):
-    def __init__(self, param_grids: List[Dict], reduction_methods: List, standard_pipeline: Pipeline, analysis_instance: Analysis) -> None:
+    def __init__(
+        self,
+        param_grids: List[Dict],
+        reduction_methods: List,
+        standard_pipeline: Pipeline,
+        analysis_instance: Analysis,
+    ) -> None:
         self.param_grids = param_grids
         self.dimensionality_reduction_methods = reduction_methods
         self.standard_pipeline = standard_pipeline
-        self.internal_var = _Variation(self.standard_pipeline, analysis_instance=analysis_instance)
+        self.internal_var = _Variation(
+            self.standard_pipeline, analysis_instance=analysis_instance
+        )
 
     def __iter__(self) -> Tuple[str, str]:
-        for param_grid, reduction_method in zip(self.param_grids, self.dimensionality_reduction_methods):
+        for param_grid, reduction_method in zip(
+            self.param_grids, self.dimensionality_reduction_methods
+        ):
             yield reduction_method, param_grid
 
     def __len__(self) -> Tuple[int, int]:
@@ -18,8 +28,11 @@ class Variations(object):
 
     def produce_variations(self) -> Dict[str, List]:
         variations = {}
-        for param_grid, reduction_method in zip(self.param_grids, self.dimensionality_reduction_methods):
+        iterator = tqdm(zip(self.param_grids, self.dimensionality_reduction_methods))
+        for param_grid, reduction_method in iterator:
             reduction_method_name = reduction_method().__class__.__name__
+            iterator.set_description(reduction_method_name)
             variations[reduction_method_name] = self.internal_var.create_variation(
-                param_grid, reduction_method, reduction_method_name)
+                param_grid, reduction_method, reduction_method_name
+            )
         return variations
